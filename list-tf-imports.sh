4a50b8d509120d6314742adfa5381454a29a3ee4
Radek Simko <radek.simko@gmail.com>
2019-08-09 16:55:43 +0100

    report test imports too

--- list-tf-imports.sh
+++ list-tf-imports.sh
@@ -3,6 +3,7 @@ REPO_PATH=$1
 
 cd $REPO_PATH
 
-ALL_IMPORTS=$(go list -json ./... | jq ". | select((.Imports | length) > 0)")
+ALL_IMPORTS=$(go list -json ./... | jq ". | select((.Imports | length) > 0 or (.TestImports | length) > 0)")
 
-echo $ALL_IMPORTS | jq -r ".Imports[] | select(startswith(\"github.com/hashicorp/terraform\"))" | xargs -I_ echo "_,$REPO_PATH"
+echo $ALL_IMPORTS | jq -r ". | select((.Imports | length) > 0) | .Imports[] | select(startswith(\"github.com/hashicorp/terraform\"))" | xargs -I_ echo "_,$REPO_PATH"
+echo $ALL_IMPORTS | jq -r ". | select((.TestImports | length) > 0) | .TestImports[] | select(startswith(\"github.com/hashicorp/terraform\"))" | xargs -I_ echo "_,$REPO_PATH"
